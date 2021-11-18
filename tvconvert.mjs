7932c5503a51e0cab1fe393929c00d1638eeba62
Soma Lucz <luczsoma@gmail.com>
2021-11-18 16:03:18 +0100

    tvconvert.mjs

--- tvconvert.mjs
+++ tvconvert.mjs
@@ -212,9 +212,7 @@ async function convert(
     "-filter:a:0",
     "loudnorm=lra=10",
     "-metadata:s:a:0",
-    "title=AAC 2.0 @ 256 kbps (normalized)",
-    "-metadata:s:a:0",
-    "language=eng",
+    "title=AAC 2.0 (normalized)",
     "-disposition:a:0",
     "default",
 

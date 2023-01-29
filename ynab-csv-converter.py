9572a179fd16f4551ef4a7c3d70afcfb2d268465
Soma Lucz <luczsoma@gmail.com>
2023-01-29 12:39:46 +0100

    ynab-csv-converter.py

--- ynab-csv-converter.py
+++ ynab-csv-converter.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import sys
+import unicodedata
 from datetime import datetime
 from decimal import Decimal
 
@@ -14,7 +15,7 @@ def read_csv(input_csv_path: str) -> list[list[str]]:
     return csv_rows
 
 
-def write_csv(input_csv_path: str, finalized_transactions: list[tuple[datetime, str, Decimal, str]]) -> None:
+def write_csv(input_csv_path: str, finalized_transactions: list[tuple[str, str, Decimal, str]]) -> None:
     input_csv_name = os.path.splitext(input_csv_path)[0]
     output_csv_path = input_csv_name + '-ynab.csv'
     with open(output_csv_path, 'w') as output_csv_file:
@@ -26,7 +27,7 @@ def write_csv(input_csv_path: str, finalized_transactions: list[tuple[datetime, 
                 [date, payee, str(amount) + ' HUF', memo])
 
 
-def get_finalized_transactions(csv_rows: list[list[str]]) -> list[tuple[datetime, str, Decimal, str]]:
+def get_finalized_transactions(csv_rows: list[list[str]]) -> list[tuple[str, str, Decimal, str]]:
     first_finalized_transaction_row_i: int | None = None
 
     for i, row in enumerate(csv_rows):
@@ -43,18 +44,24 @@ def get_finalized_transactions(csv_rows: list[list[str]]) -> list[tuple[datetime
                   reverse=True)
 
 
-def map_csv_row_to_finalized_transaction(csv_row: list[str]) -> tuple[datetime, str, Decimal, str]:
+def map_csv_row_to_finalized_transaction(csv_row: list[str]) -> tuple[str, str, Decimal, str]:
     type_field, date_1_field, date_2_field, id_field, amount_field, \
         notice_1_field, notice_2_field, *notices_rest = csv_row
 
     if (type_field == 'Kártyatranzakció'):
-        date = datetime.strptime(notice_1_field.split(' ')[1], '%Y%m%d')
+        parsed_date = datetime.strptime(notice_1_field.split(' ')[1], '%Y%m%d')
     else:
-        date = datetime.strptime(date_1_field.split(',')[0], '%Y.%m.%d.')
+        parsed_date = datetime.strptime(
+            date_1_field.split(',')[0], '%Y.%m.%d.')
 
-    date = datetime.strftime(date, '%Y-%m-%d')
+    date = datetime.strftime(parsed_date, '%Y-%m-%d')
     payee = notice_2_field
-    amount = Decimal(amount_field.split(' ')[0].replace(',', '.'))
+    amount = Decimal(
+        unicodedata.normalize('NFKC', amount_field)
+        .replace('HUF', '')
+        .replace(' ', '')
+        .replace(',', '.')
+    )
     memo = ', '.join(
         filter(lambda x: x != '', [type_field, notice_1_field] + notices_rest))
     return (date, payee, amount, memo)
@@ -63,7 +70,7 @@ def map_csv_row_to_finalized_transaction(csv_row: list[str]) -> tuple[datetime, 
 def main() -> None:
     input_csv_path: str = sys.argv[1]
     csv_rows: list[list[str]] = read_csv(input_csv_path)
-    finalized_transactions: list[tuple[datetime, str,
+    finalized_transactions: list[tuple[str, str,
                                        Decimal, str]] = get_finalized_transactions(csv_rows)
     write_csv(input_csv_path, finalized_transactions)
 
